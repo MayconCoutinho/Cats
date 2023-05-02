73bc7460ee4034bbb2483fb6c90101b08e62650e
MayconCoutinho <mayconcoutinhoart@gmail.com>
2023-05-02 16:57:30 -0300

    arrumando responsivo do imagensGrid

--- src/components/imagensGrid/index.js
+++ src/components/imagensGrid/index.js
@@ -1,26 +1,32 @@
-import styles from '../../sass/Imagens.module.scss'
+import Image from "next/image";
+import styles from "../../sass/Imagens.module.scss";
 
 export const ImagensGrid = ({ imagem }) => {
     const MonstraImg = () => {
-        const img = imagem.map(item => {
-
+        const img = imagem.map((item) => {
             return item.map((item) => {
                 return (
-                    <figure className={styles.figure} >
-                        <img alt='' className={styles.imagem} src={item} />
+                    // eslint-disable-next-line react/jsx-key
+                    <figure className={styles.figure}>
+                        <Image
+                            alt=""
+                            className={styles.imagem}
+                            src={item}
+                            width="100%"
+                            height="100%"
+                            layout="responsive"
+                        />
                     </figure>
-                )
-            })
-        }
-        )
-        return img
-    }
-    return (
-        imagem.length <= 0 ? <h1> Esperando imagens </h1> :
-
-            <div className={styles.container}>
-                <MonstraImg />
-            </div>
-    )
-
-}
+                );
+            });
+        });
+        return img;
+    };
+    return imagem.length <= 0 ? (
+        <h1> Esperando imagens </h1>
+    ) : (
+        <div className={styles.container}>
+            <MonstraImg />
+        </div>
+    );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,35 @@
-import { useContext, useEffect } from 'react'
-import styles from '../sass/Imagens.module.scss'
-import { Header } from '../components/header'
-import { GlobalContext } from '../context'
-import { ImagensGrid } from '../components/imagensGrid'
-import { InfoHead } from '../components/infoHead'
+import { useContext, useEffect } from "react";
+import styles from "../sass/Imagens.module.scss";
+import { Header } from "../components/header";
+import { GlobalContext } from "../context";
+import { ImagensGrid } from "../components/imagensGrid";
+import { InfoHead } from "../components/infoHead";
 
 export default function Home() {
-    const { gatos, atualizar } = useContext(GlobalContext)
+    const { gatos, atualizar } = useContext(GlobalContext);
 
     useEffect(() => {
         const intersectionObserver = new IntersectionObserver(() => {
-            atualizar()
-        })
+            atualizar();
+        });
 
-        intersectionObserver.observe(document.querySelector('#sentinela'))
-        return () => intersectionObserver.disconnect()
-    }, [])
+        intersectionObserver.observe(document.querySelector("#sentinela"));
+        return () => intersectionObserver.disconnect();
+    }, []);
 
     return (
         <>
-            <InfoHead title={"Pinterest"} meta_content={"Pinterest"} meta_name={"Pinterest"}/>
+            <InfoHead
+                title={"Pinterest"}
+                meta_content={"Pinterest"}
+                meta_name={"Pinterest"}
+            />
 
             <Header />
 
             <ImagensGrid imagem={gatos} />
 
-            <li id='sentinela' className={styles.sentinela} />
+            <li id="sentinela" className={styles.sentinela} />
         </>
-    )
+    );
 }
